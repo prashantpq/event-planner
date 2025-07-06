ef1ad465f273ac0ea2382a79d311c823b162d9bf
Prashant Sharma <prashant.ps9833@gmail.com>
2025-07-06 21:07:03 +0530

    changes

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path : str) -> List[str]:
+    """
+    This function will return the list of requirements
+    """
+
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [r.strip() for r in requirements]
+    return requirements
+
 
 setup(
     name = 'event_planner',
-    version = '0.1.0',
+    version = '0.0.1',
     packages = find_packages(),
-    install_requires = [
-        'langgraph',
-        'langchain',
-        'langchain-core',
-        'langchain-community',
-        'python-dotenv',
-        'langchain-groq'
-
-    ],
+    install_requires = get_requirements('./requirements.txt'),
     author = 'Prashant Sharma',
     author_email = 'prashant.ps9833@gmail.com',
     description = 'A multi-agent AI event & travel planner system',
